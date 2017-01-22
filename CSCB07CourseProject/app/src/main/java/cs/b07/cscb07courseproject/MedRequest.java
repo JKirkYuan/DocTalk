6c0054d887212dc972481d583f087cdbbb61a47a
Vincent Teng <vincent@Vincents-MacBook-Pro.local>
2017-01-21 20:12:26 -0500

    Modifying patient request page

---
+++ CSCB07CourseProject/app/src/main/java/cs/b07/cscb07courseproject/MedRequest.java
@@ -0,0 +1,34 @@
+package cs.b07.cscb07courseproject;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import backend.Doctor;
+import backend.Patient;
+import backend.Driver;
+/**
+ * Created by vincent on 2017-01-21.
+ */
+
+public class MedRequest extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_homepage);
+
+        Intent intent = getIntent();
+
+
+        //setUpPage();
+    }
+
+
+}
--- CSCB07CourseProject/app/src/main/java/cs/b07/cscb07courseproject/PatientHomepage.java
+++ CSCB07CourseProject/app/src/main/java/cs/b07/cscb07courseproject/PatientHomepage.java
@@ -51,74 +51,19 @@ public class PatientHomepage extends AppCompatActivity {
 
             }
         });
-
-        //setUpPage();
     }
+
     public void onNewIntent(Intent intent){
         // grab all the necessary data for a refresh and redo the page
 
         this.patient = (Patient) intent.getSerializableExtra("patient");
         this.driver = (Driver) intent.getSerializableExtra("driver");
-
-        //setUpPage();
     }
 
-//    public void setUpPage(){
-//        ArrayList<String> items = new ArrayList<>();
-//        result = patient.getBookedItineraries();
-//        ArrayList<Trip> trips;
-//        // grab all the text for the list items
-//        for(Itinerary i: result) {
-//
-//            // here to format this, we want a rows of itinerary information
-//            String text = "";
-//            trips = i.getTripList();
-//            for (Trip trip : trips) {
-//                // first we update the patient's itineraries to reflect changed information
-//                Flight f = (Flight) trip;
-//                trip = (Trip) driver.getFlightDatabase().getFlight(f.getFlightNumber());
-//                // then we grab all the flight information and put it into the file
-//                text += "Departs from: " + trip.getOrigin();
-//                text += " at " + trip.getDepartureTime();
-//                text += " to arrive at " + trip.getDestination();
-//                text += " at " + trip.getArrivalTime();
-//                // then we place and -> to show that the flights are connected
-//                text += " -> ";
-//            }
-//            text += " , Trip will take: " + i.getTotalTime();
-//            text += " , Number of free seats: " + i.getLowestNumSeats();
-//            text += " , Will cost in total: " + i.getTotalCost();
-//            items.add(text);
-//        }
-//        lv = (ListView) findViewById(R.id.booked_flights);
-//        // Adding items to ListView
-//        adapter = new ArrayAdapter<String>(PatientHomepage.this, R.layout.list_item, R.id.item_name, items);
-//        lv.setAdapter(adapter);
-//        lv.setClickable(true);
-//        adapter.setNotifyOnChange(true);
-//        lv.setOnTouchListener(new ListView.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                int action = event.getAction();
-//                switch (action) {
-//                    case MotionEvent.ACTION_DOWN:
-//                        // Disallow ScrollView to intercept touch events.
-//                        v.getParent().requestDisallowInterceptTouchEvent(true);
-//                        break;
-//
-//                    case MotionEvent.ACTION_UP:
-//                        // Allow ScrollView to intercept touch events.
-//                        v.getParent().requestDisallowInterceptTouchEvent(false);
-//                        break;
-//                }
-//
-//                // Handle ListView touch events.
-//                v.onTouchEvent(event);
-//                return true;
-//            }
-//        });
-//
-//    }
+
+    public void makeMedRequest(View v){
+
+    }
 
     public void Logout(View v) {
         Intent intentLogout = new Intent(this, Login.class);
